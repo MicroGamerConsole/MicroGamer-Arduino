871cf51f897d92694d2facb13a57741579936757
Fabien Chouteau <fabien.chouteau@gmail.com>
2019-01-26 17:07:19 +0100

    Fix the memory card example

--- examples/MemoryCard/MemoryCard/MemoryCard.ino
+++ examples/MemoryCard/MemoryCard/MemoryCard.ino
@@ -6,44 +6,41 @@ MicroGamer mg;
 
 void setup() {
   mg.boot();
+  mg.enableDoubleBuffer();
   mg.setFrameRate(30);
 }
 
 void loop() {
-//  unsigned int *ptr = (unsigned int *)mem.data();
+  unsigned int *ptr = (unsigned int *)mem.data();
 
   if (!mg.nextFrame()) {
     return; // go back to the start of the loop
   }
 
   mg.clear();
-  mg.drawLine(0, 0, WIDTH - 1, HEIGHT - 1);
-  mg.drawLine(WIDTH - 1, 0, 0, HEIGHT - 1);
   mg.setCursor(0, 0);
-  mg.println("Data: 0x");
-  //mg.print(*ptr, HEX);
+  mg.print("Data: 0x");
+  mg.print(*ptr, HEX);
   mg.print("\n");
   mg.print("RAM Addr: 0x");
-  mg.drawLine(5, 0, WIDTH - 5, HEIGHT - 1);
-  mg.drawLine(WIDTH - 5, 0, 5, HEIGHT - 1);
-  //mg.print((unsigned int)ptr, HEX);
+  mg.print((unsigned int)ptr, HEX);
   mg.print("\n");
   mg.println("Press:");
   mg.println(" - UP to increment");
   mg.println(" - DOWN to decrement");
-  mg.println(" - A to load the data");
-  mg.println(" - B to save the data");
+  mg.println(" - Y to load the data");
+  mg.println(" - X to save the data");
   mg.display();
 
-//  if (mg.pressed(UP_BUTTON)) {
-//    (*ptr)++;
-//  } else if (mg.pressed(DOWN_BUTTON)){
-//    (*ptr)--;
-//  }
-//
-//  if (mg.pressed(A_BUTTON)) {
-//    mem.load();
-//  } else if (mg.pressed(B_BUTTON)){
-//    mem.save();
-//  }
+  if (mg.pressed(UP_BUTTON)) {
+    (*ptr)++;
+  } else if (mg.pressed(DOWN_BUTTON)){
+    (*ptr)--;
+  }
+
+  if (mg.pressed(Y_BUTTON)) {
+    mem.load();
+  } else if (mg.pressed(X_BUTTON)){
+    mem.save();
+  }
 }
